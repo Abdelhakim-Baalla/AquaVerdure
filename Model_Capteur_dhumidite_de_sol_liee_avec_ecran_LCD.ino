e549df594dd889b94f5c243aca3e1ae9324e2f8c
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-08-16 15:13:18 +0100

    Changer l'inistialisation et installer nouvell bibliotheque

--- Model_Capteur_dhumidite_de_sol_liee_avec_ecran_LCD.ino
+++ Model_Capteur_dhumidite_de_sol_liee_avec_ecran_LCD.ino
@@ -1,23 +1,8 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <LiquidCrystal_I2C.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &wire, OLED_RESET);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const int capteurHumidite = A0;
-const int seuilHumiditeB = 40;
-const int seuilHumiditeH = 85;
+int sensorPin = A0; 
+int sensorValue = 0;
 
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
