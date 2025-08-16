3d39fea2b9c494f2103d10289cae626830a7c5aa
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-08-16 15:20:48 +0100

    ajouter dans la fonctionne loop qui checker chaque seconde l'humidité et d'apres les mesure du capteur on ecrit sur l'ecrant le resultat

--- Model_Capteur_dhumidite_de_sol_liee_avec_ecran_LCD.ino
+++ Model_Capteur_dhumidite_de_sol_liee_avec_ecran_LCD.ino
@@ -13,3 +13,23 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("Humidite du sol: ");
 }
+
+
+void loop() {
+  sensorValue = analogRead(sensorPin); 
+
+  Serial.print("Humidite du sol: ");
+  Serial.println(sensorValue);
+
+  lcd.setCursor(0, 1); 
+
+  if (sensorValue > 700) {  // Sol sec
+    lcd.print("Sec   ");
+  } else if (sensorValue > 400) { // Humidite moyenne
+    lcd.print("Humide");
+  } else {  // Sol tres humide
+    lcd.print("Mouille");
+  }
+
+  delay(1000); // Attente 1 seconde
+}
